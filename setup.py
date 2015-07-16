27e605ffa22fcb028e9998dd514ab3899e3541da
TonyBarnett <tony.barnett@ukplc.net>
2015-07-16 09:44:16 +0100

    first stab at a setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name="utility functions",
+    packages=["utility_functions"],
+    version="0.1",
+    description="utility functions",
+    long_description="utility functions",
+    author="me",
+    author_email="tony.barnett@cloudbuy.com",
+    url="localhost",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ],
+)
