94d160d60ae8bd08d8df735008545e45d49fe471
TonyBarnett <tony.barnett@ukplc.net>
2015-08-20 16:06:16 +0100

    add a minimum value of y to r_squared calculations

--- utility_functions/cartesian_plot_functions.py
+++ utility_functions/cartesian_plot_functions.py
@@ -3,10 +3,13 @@ def _get_yt(y: list) -> float:
     return yt / len(y)
 
 
-def _get_y_minus_y_hat(x: list, y: list, a: float, b: float) -> float:
+def _get_y_minus_y_hat(x: list, y: list, a: float, b: float, min_y: float=None) -> float:
     y_minus_y_hat = 0
     for i, y_i in enumerate(y):
-        y_minus_y_hat += (y_i - (a * x[i] + b)) ** 2
+        if min_y is not None and (a * x[i] + b) < min_y:
+            y_minus_y_hat += min_y
+        else:
+            y_minus_y_hat += (y_i - (a * x[i] + b)) ** 2
     return y_minus_y_hat
 
 
@@ -17,7 +20,7 @@ def _get_y_minus_y_bar(y: list, yt: float) ->float:
     return y_minus_y_bar
 
 
-def get_r_squared(x: list, y: list, a: float, b: float) -> float:
+def get_r_squared(x: list, y: list, a: float, b: float, minimum_value_y: float=None) -> float:
     y_minus_y_hat = _get_y_minus_y_hat(x, y, a, b)
     yt = _get_yt(y)
     y_minus_y_bar = _get_y_minus_y_bar(y, yt)
